a7854d780183cd6f06537b5bd047c0a351e09f3b
wrgoto <wrgoto@gmail.com>
2023-08-24 10:19:32 -0700

    Add platform and update swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
 	name: "ActiveWin",
+	platforms: [
+		.macOS(.v13)
+	],
 	products: [
 		.executable(
 			name: "active-win",
